e98fa1e2ec0e09dc56ac7bb6132e97ff9fb5777c
nir sivan <nirsivan204@gmail.com>
2023-02-03 01:27:23 +0200

    resource manager

--- Assets/Resources/Scripts/ResourceManager.cs
+++ Assets/Resources/Scripts/ResourceManager.cs
@@ -1,22 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 public static class ResourceManager
 {
-    static int _waterAmount;
-    static int _treesAmount;
+    static float _waterAmount;
+    //static int _treesAmount;
     
-    public static int WaterAmount { get => _waterAmount; set => _waterAmount = value; }
-    public static int TreesAmount { get => _treesAmount; set => _treesAmount = value; }
+    public static float WaterAmount { get => _waterAmount; set => _waterAmount = value; }
+   //public static int TreesAmount { get => _treesAmount; set => _treesAmount = value; }
 
-    public static void AddWater(int amount)
+    public static float GetWaterAmount()
+    {
+        return _waterAmount;
+    }
+
+    public static void AddWater(float amount)
     {
         _waterAmount += amount;
     }
-    public static void AddTrees(int amount)
+
+    public static void RemoveWater(float amount)
     {
-        _treesAmount += amount;
+        _waterAmount -= amount;
     }
 
 
