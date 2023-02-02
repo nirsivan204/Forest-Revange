55950e255249103882933ef9e6409dd1b1853771
AmitReuveni <amitreuveni@gmail.com>
2023-02-02 23:37:23 +0200

    Added start of rootagent script and some files for rider solution (.idea)

---
+++ Assets/Scripts/RootAgent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class RootAgent : MonoBehaviour
+{
+    [SerializeField] private Transform goal;
+    [SerializeField] private Camera mainCamera;
+
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+
+        }
+    }
+}
