90504dc5b6d3e671bedb29bb1c5090853ebb05b5
AmitReuveni <amitreuveni@gmail.com>
2023-02-03 15:41:44 +0200

    Added basic tree script

--- Assets/Resources/Scripts/TreeScript.cs
+++ Assets/Resources/Scripts/TreeScript.cs
@@ -1,3 +1,43 @@
-﻿public class TreeScript
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TreeScript : MonoBehaviour
 {
+    private int waterAmount = 0;
+    private int stage = 1;
+    private UnityEvent growTreeEvent;
+    private List<ResourceEntity> waterResources;
+
+    private void GrowTree()
+    {
+        stage++;
+        growTreeEvent.Invoke();
+    }
+
+    private void CollectWater(int amount)
+    {
+        waterAmount += amount;
+    }
+
+    private bool UseWater(int amount)
+    {
+        if (waterAmount >= amount)
+        {
+            waterAmount -= amount;
+            return true;
+        }
+
+        return false;
+    }
+
+    public void AddWaterResource(ResourceEntity resource)
+    {
+        waterResources.Add(resource);
+    }
+
+    public void AddGrowTreeEventListener(UnityAction action)
+    {
+        growTreeEvent.AddListener(action);
+    }
 }
