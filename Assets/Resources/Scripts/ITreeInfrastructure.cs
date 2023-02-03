887bdfd67154edf856fcc3160359da7a9d9db1a1
AmitReuveni <amitreuveni@gmail.com>
2023-02-03 15:41:34 +0200

    Added basic tree infrastructure

---
+++ Assets/Resources/Scripts/ITreeInfrastructure.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Resources.Scripts
+{
+    public interface ITreeInfrastructure
+    {
+        public void AddTree(TreeScript tree, TreeScript fatherTree = null);
+
+        public TreeScript GetFatherOfTree(TreeScript tree);
+
+        public List<TreeScript> GetChildrenOfTree(TreeScript tree);
+    }
+}
---
+++ Assets/Resources/Scripts/TreeInfrastructure.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Resources.Scripts
+{
+    public class TreeInfrastructure : MonoBehaviour, ITreeInfrastructure
+    {
+        public void AddTree(TreeScript tree, TreeScript fatherTree = null)
+        {
+            
+        }
+
+        public TreeScript GetFatherOfTree(TreeScript tree)
+        {
+            // return tree.
+            return null;
+        }
+
+        public List<TreeScript> GetChildrenOfTree(TreeScript tree)
+        {
+            
+            return null;
+        }
+    }
+}
