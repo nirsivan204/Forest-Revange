fb894d4e1bae3e2e6cfa3b27f5f571cf466b2fdf
nir sivan <nirsivan204@gmail.com>
2023-02-04 13:01:52 +0200

    explosions

--- Assets/Resources/Scripts/AppleDestruction.cs
+++ Assets/Resources/Scripts/AppleDestruction.cs
@@ -1,21 +1,49 @@
+using System.Collections;
 using UnityEngine;
 
 public class AppleDestruction : MonoBehaviour
 {
     public ResourceTypes resourceType;
+    public ApplePlanter plant;
+    GameObject hitParticle = null; 
 
     private void OnCollisionEnter(Collision collision)
     {
         string powerString = powerToString();
         if (collision.gameObject.name.IndexOf(powerString) != -1)
         {
+            plant.canBePlanted = false;
             Destroy(collision.gameObject);
-            //make effect
+            StartCoroutine(PlayEffect());
         }
-        else
+    }
+
+    IEnumerator PlayEffect()
+    {
+        switch (resourceType)
         {
-            Destroy(this);
+            case ResourceTypes.Water:
+                break;
+            case ResourceTypes.Gas:
+                hitParticle = (GameObject)Resources.Load("Particle Effects/BigExplosion.prefab");
+                break;
+            case ResourceTypes.Sewage:
+                hitParticle = (GameObject)Resources.Load("Particle Effects/SwampBall.prefab");
+                break;
+            case ResourceTypes.Electricity:
+                break;
+
         }
+        if (hitParticle != null)
+        {
+            Instantiate(hitParticle, this.transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(hitParticle.GetComponent<ParticleSystem>().main.duration);
+        }
+        Destroy(this.gameObject);
+
+
+        //make effect
+
     }
 
     private string powerToString() {
@@ -23,7 +51,7 @@ public class AppleDestruction : MonoBehaviour
             case ResourceTypes.Gas:
                 return "gas";
             case ResourceTypes.Sewage:
-                return "sewage";
+                return "acid";
             case ResourceTypes.Electricity:
                 return "electricity";
             case ResourceTypes.Water:
